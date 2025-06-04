88ed2e10027d62c0c699896b48b5dd60731c2d13
DGTSI <dgtsifgjcdmx@gmail.com>
2025-06-04 13:53:29 -0600

    primer commit

---
+++ src/main/java/fgjcdmx/gob/vehiculos_back/VehiculosBackApplication.java
@@ -0,0 +1,13 @@
+package fgjcdmx.gob.vehiculos_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fgjcdmx/gob/vehiculos_back/VehiculosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fgjcdmx.gob.vehiculos_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
